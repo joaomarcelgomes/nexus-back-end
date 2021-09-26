f45cea884e488178b0558a113e01cfedccec2786
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-26 14:22:00 -0300

    Adding entity to generate Id with Guid

---
+++ Backend.Domain.Shared/Entities/Entity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Backend.Domain.Shared.Entities
+{
+    public abstract class Entity
+    {
+        protected Entity() => Id = Guid.NewGuid();
+
+        public Guid Id { get; private set; }
+    }
+}
