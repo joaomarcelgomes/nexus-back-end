35a9d5df34f2f402112af271f8c1777a47381996
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-26 16:35:17 -0300

    Adding repository from user

---
+++ Backend.Domain.Infra/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Backend.Domain.Entities;
+using Backend.Domain.Infra.Data;
+using Backend.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Domain.Infra.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context) => _context = context;
+
+        public async void Create(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> CheckEmail(string email) 
+            => await _context
+                .Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Email == email) == null;
+    }
+}
