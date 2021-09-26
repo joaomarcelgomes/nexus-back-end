181b7b8d3f539a3e51f9b03c89f8f06a5b28f105
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-26 16:34:57 -0300

    Adding data context

---
+++ Backend.Domain.Infra/Data/DataContext.cs
@@ -0,0 +1,21 @@
+using Backend.Domain.Entities;
+using Backend.Domain.Infra.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Domain.Infra.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<User> Users { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            new UserMapping().Configure(builder.Entity<User>());
+        }
+    }
+}
