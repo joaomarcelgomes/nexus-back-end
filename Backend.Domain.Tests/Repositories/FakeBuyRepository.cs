0196b768ff7a3d2a212d46bb84948e5a2c0f0039
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-27 12:00:30 -0300

    Adding search at buy repository

--- Backend.Domain.Tests/Repositories/FakeBuyRepository.cs
+++ Backend.Domain.Tests/Repositories/FakeBuyRepository.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Backend.Domain.Entities;
 using Backend.Domain.Repositories;
+using Backend.Domain.Shared.Pagination;
 
 namespace Backend.Domain.Tests.Repositories
 {
@@ -7,6 +11,16 @@ namespace Backend.Domain.Tests.Repositories
     {
         public void Create(Buy buy)
         {
+        }
+
+        public Task<List<Buy>> FindAll(PaginationFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Buy> FindById(PaginationFilter filter, Guid id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Backend.Domain/Repositories/IBuyRepository.cs
+++ Backend.Domain/Repositories/IBuyRepository.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Backend.Domain.Entities;
+using Backend.Domain.Shared.Pagination;
 
 namespace Backend.Domain.Repositories
 {
     public interface IBuyRepository
     {
         public void Create(Buy buy);
+        public Task<List<Buy>> FindAll(PaginationFilter filter);
+        public Task<Buy> FindById(PaginationFilter filter, Guid id);
     }
 }
