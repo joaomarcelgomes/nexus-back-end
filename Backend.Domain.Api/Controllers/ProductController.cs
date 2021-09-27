102012c9515c2b5499e58a8b6e55adcebe7ae853
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-27 08:43:50 -0300

    Adding controller to find products

--- Backend.Domain.Api/Controllers/ProductController.cs
+++ Backend.Domain.Api/Controllers/ProductController.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections;
+using System.Threading.Tasks;
 using Backend.Domain.Commands;
 using Backend.Domain.Commands.ProductCommands;
 using Backend.Domain.Handlers;
+using Backend.Domain.Repositories;
+using Backend.Domain.Shared.Pagination;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +29,29 @@ namespace Backend.Domain.Api.Controllers
             catch(Exception)
             {
                 return BadRequest(new GenericCommandResult(false, "Error in try to create product", command));
+            }
+        }
+
+        [HttpGet("results")]
+        [AllowAnonymous]
+        public async Task<ActionResult<GenericCommandResult>> Results([FromServices] IProductRepository repository,[FromQuery] PaginationFilter filter, [FromQuery] string search)
+        {
+            try
+            {
+                PaginationFilter paginationFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
+
+                var products = string.IsNullOrEmpty(search)
+                    ? await repository.FindAll(paginationFilter)
+                    : await repository.FindByName(paginationFilter, search);
+
+                if(products.Count == 0)
+                    return NotFound(new GenericCommandResult(false, "No products were found", new ArrayList()));
+
+                return Ok(new GenericCommandResult(true, "Products", products));
+            }
+            catch (Exception)
+            {
+                return BadRequest(new GenericCommandResult(false, "Error in try to query the product(s)", new ArrayList()));
             }
         }
     }
