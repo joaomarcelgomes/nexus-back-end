1628054c96560d328b5d2ec60955b31133b5e800
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-26 21:06:29 -0300

    Adding mapping from product to create table

---
+++ Backend.Domain.Infra/Mapping/ProductMapping.cs
@@ -0,0 +1,19 @@
+using Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend.Domain.Infra.Mapping
+{
+    public class ProductMapping : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(64).HasColumnType("nvarchar(64)");
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(64).HasColumnType("nvarchar(64)");
+            builder.Property(p => p.Price).IsRequired();
+            builder.Property(p => p.Amount).IsRequired();
+        }
+    }
+}
