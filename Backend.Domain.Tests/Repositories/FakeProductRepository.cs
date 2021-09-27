f0c0f51d40d9cb95d6c2d7611ad4a059251067bc
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-27 08:42:18 -0300

    Adding method to create and find products

--- Backend.Domain.Tests/Repositories/FakeProductRepository.cs
+++ Backend.Domain.Tests/Repositories/FakeProductRepository.cs
@@ -1,5 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Backend.Domain.Entities;
 using Backend.Domain.Repositories;
+using Backend.Domain.Shared.Pagination;
 
 namespace Backend.Domain.Tests.Repositories
 {
@@ -7,6 +11,16 @@ namespace Backend.Domain.Tests.Repositories
     {
         public void Create(Product product)
         {
+        }
+
+        public Task<List<Product>> FindAll(PaginationFilter filter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Product>> FindByName(PaginationFilter filter, string query)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- Backend.Domain/Repositories/IProductRepository.cs
+++ Backend.Domain/Repositories/IProductRepository.cs
@@ -1,9 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Backend.Domain.Entities;
+using Backend.Domain.Shared.Pagination;
 
 namespace Backend.Domain.Repositories
 {
     public interface IProductRepository
     {
         public void Create(Product product);
+        public Task<List<Product>> FindAll(PaginationFilter filter);
+        public Task<List<Product>> FindByName(PaginationFilter filter, string query);
     }
 }
