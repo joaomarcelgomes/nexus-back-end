663ad380fcb676c54b87aa2c4cf45448c47d892e
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-27 12:01:37 -0300

    Impl searchs

--- Backend.Domain.Infra/Repositories/BuyRepository.cs
+++ Backend.Domain.Infra/Repositories/BuyRepository.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Backend.Domain.Entities;
 using Backend.Domain.Infra.Data;
+using Backend.Domain.Queries;
 using Backend.Domain.Repositories;
+using Backend.Domain.Shared.Pagination;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Domain.Infra.Repositories
 {
@@ -15,5 +22,23 @@ namespace Backend.Domain.Infra.Repositories
             await _context.Buys.AddAsync(buy);
             await _context.SaveChangesAsync();
         }
+
+        public async Task<List<Buy>> FindAll(PaginationFilter filter)
+            => await _context
+                .Buys
+                .AsNoTracking()
+                .Skip((filter.PageNumber -1) * filter.PageSize)
+                .Take(filter.PageSize)
+                .ToListAsync();
+
+        public async Task<Buy> FindById(PaginationFilter filter, Guid id)
+            => await _context
+                .Buys
+                .Include(b => b.Products)
+                .Where(BuyQueries.FindById(id))
+                .Skip((filter.PageNumber -1) * filter.PageSize)
+                .Take(filter.PageSize)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
     }
 }
