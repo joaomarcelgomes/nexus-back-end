533e8b1834441eaea59a42b5ef3df51335de64e5
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-09-28 08:05:03 -0300

    Put public methods

--- Backend.Domain/Repositories/IUserRepository.cs
+++ Backend.Domain/Repositories/IUserRepository.cs
@@ -5,8 +5,8 @@ namespace Backend.Domain.Repositories
 {
     public interface IUserRepository
     {
-        void Create(User user);
-        Task<bool> CheckEmail(string email);
-        Task<User> Login(string email, string password);
+        public void Create(User user);
+        public Task<bool> CheckEmail(string email);
+        public Task<User> Login(string email, string password);
     }
 }
