8e177e3cd7a3ec4629b7ef153daaece914e71d1a
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2021-10-13 16:59:07 -0300

    Adding image support

--- Backend.Domain.Api/Controllers/ProductController.cs
+++ Backend.Domain.Api/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Backend.Domain.Commands;
 using Backend.Domain.Commands.ProductCommands;
@@ -7,14 +9,19 @@ using Backend.Domain.Handlers;
 using Backend.Domain.Repositories;
 using Backend.Domain.Shared.Pagination;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Domain.Api.Controllers
-{
+{    
     [ApiController]
     [Route("v1/product")]
     public class ProductController : ControllerBase
     {
+        public static IWebHostEnvironment _environment;
+        public ProductController(IWebHostEnvironment environment) => _environment = environment;
+
         [HttpPost("create")]
         [Authorize(Roles = "Admin")]
         public ActionResult<GenericCommandResult> Create([FromServices] ProductHandler handler,
@@ -29,6 +36,30 @@ namespace Backend.Domain.Api.Controllers
             catch(Exception)
             {
                 return BadRequest(new GenericCommandResult(false, "Error in try to create product", command));
+            }
+        }
+
+        [HttpPost("create/image")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<GenericCommandResult>> CreateImage([FromServices] ProductHandler handler,
+            [FromForm] IFormFile file)
+        {
+            try
+            {
+                var directory = _environment.WebRootPath + "../images/";
+
+                if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+
+                using (FileStream filestream = System.IO.File.Create(directory + file.FileName))
+                {
+                    await file.CopyToAsync(filestream);
+                    filestream.Flush();
+                    return Ok(new GenericCommandResult(true, "Image saved successfully", file));
+                }
+            }
+            catch(Exception)
+            {
+                return BadRequest(new GenericCommandResult(false, "Error in try to save image", file));
             }
         }
 
@@ -53,6 +84,18 @@ namespace Backend.Domain.Api.Controllers
             {
                 return BadRequest(new GenericCommandResult(false, "Error in try to query the product(s)", new ArrayList()));
             }
+        }
+
+        [HttpGet("result/image/{id}")]
+        [AllowAnonymous]
+        public ActionResult<GenericCommandResult> ResultImage(string id)
+        {
+            Byte[] file = System.IO.File.ReadAllBytes(_environment.WebRootPath + "../images/" + id);
+
+            if(file.Length > 0)
+                return Ok(new GenericCommandResult(true, "Image", File(file, "image/jpeg")));
+ 
+            return BadRequest(new GenericCommandResult(false, "Image not found", new Byte()));
         }
     }
 }
